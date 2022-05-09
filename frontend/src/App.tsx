085898c64a03cf626d63f0e284816f2fa49193d6
livioara <assislivio@gmail.com>
2022-05-09 20:58:54 -0300

    1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {ReactComponent as gitimg} from './assets/img/git.png'
 
 
 function App() {
   return (
-  <h1>Página Principal</h1>
+  
+    <header>
+      <nav>
+        <div>
+          <h1>DSFilmes</h1>
+          <a href=""></a>
+        </div>
+      </nav>
+    </header>
+  
   );
 }
 
